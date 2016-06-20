c6d9575eac2f8da0dcefbb690c92a5a610e50f7f
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-20 16:20:29 +0200

    001/js more fp solution

--- javascript/prob001/prob001.js
+++ javascript/prob001/prob001.js
@@ -1,4 +1,4 @@
-use "strict"
+"use strict"
 
 /**
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -8,7 +8,8 @@ var divisible = function(n, k) {
 	return n % k == 0;
 }
 
-var limit = 10;
+// trivial solution
+var limit = 1000;
 var sum = 0;
 
 for (var i=0; i<limit; i++) {
@@ -18,3 +19,15 @@ for (var i=0; i<limit; i++) {
 }
 
 console.log(sum);
+// 233168
+
+// more fp solution
+var N = 1000; 
+var result = Array.apply(null, {length: N}).map(Number.call, Number)
+	.filter(function(n) {
+		return divisible(n,3) || divisible(n,5);
+	}).reduce(function(acc, curr) {
+		return acc + curr;
+	}, 0);
+
+console.log(result);
