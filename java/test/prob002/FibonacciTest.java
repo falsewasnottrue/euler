06ed335797f8a8aed358912c4f4d36c78eb587ef
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-24 16:41:36 +0200

    java: fibonacci test

---
+++ java/test/prob002/FibonacciTest.java
@@ -0,0 +1,16 @@
+package prob002;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class FibonacciTest {
+
+    @Test
+    public void testStream() {
+        Stream<Long> fibs = Fibonacci.stream();
+        assertArrayEquals(new Long[] {1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L}, fibs.limit(10).toArray());
+    }
+}
