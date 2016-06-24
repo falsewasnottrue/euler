13b57221536c26696089d2e96898a3a72d5f4c7f
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-24 16:18:57 +0200

    004 java

---
+++ java/src/prob004/Palindrom.java
@@ -0,0 +1,10 @@
+package prob004;
+
+public class Palindrom {
+
+    public static boolean isPalindrom(int n) {
+        String s = String.valueOf(n);
+        String r = new StringBuilder(s).reverse().toString();
+        return s.equals(r);
+    }
+}
---
+++ java/src/prob004/Prob004.java
@@ -0,0 +1,24 @@
+package prob004;
+
+public class Prob004 {
+
+    /**
+     * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+     */
+    public static void main(String... args) {
+
+        int max = 0;
+        for (int i=100; i<1000; i++) {
+            for (int j=100; j<1000; j++) {
+                int k = i*j;
+                if (k > max && Palindrom.isPalindrom(k)) {
+                    System.out.println(i + " * " + j + " = " +k);
+                    max = k;
+                }
+            }
+        }
+
+        // 913 * 993 = 906609
+    }
+}
