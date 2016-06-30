188bbf4034718414a858498c8b6ae690bc3ba181
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-06-30 10:55:39 +0200

    Multisets

---
+++ java/src/prob005/MultiSet.java
@@ -0,0 +1,42 @@
+package prob005;
+
+import java.util.*;
+import java.util.function.BiFunction;
+
+public class MultiSet<T> {
+
+    private final Map<T, Integer> occurrences = new HashMap<>();
+
+    public MultiSet() {}
+
+    public MultiSet(final Collection<T> c) {
+        for (final T elem : c) { put(elem); }
+    }
+
+    public int size() {
+        return occurrences.size();
+    }
+
+    public void put(final T elem) {
+        occurrences.put(elem, occurrences.getOrDefault(elem, 0) + 1);
+    }
+
+    public int get(final T elem) {
+        return occurrences.getOrDefault(elem, 0);
+    }
+
+    public MultiSet<T> union(final MultiSet<T> other, final BiFunction<Integer, Integer, Integer> combiner) {
+        final MultiSet<T> result = new MultiSet<T>();
+        final Set<T> keys = new HashSet<T>(occurrences.keySet());
+        keys.addAll(other.occurrences.keySet());
+
+        for (final T elem : keys) {
+            final int count = combiner.apply(get(elem), other.get(elem));
+            for (int i=0; i < count; i++) {
+                result.put(elem);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ java/src/prob005/Prob005.java
@@ -0,0 +1,18 @@
+package prob005;
+
+/**
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+public class Prob005 {
+
+    public static void main(String... args) {
+        final int N = 10;
+
+        
+        // 1. factorize
+        // 2. multiset
+        // 3. multiset union
+        // 4. product
+    }
+}
