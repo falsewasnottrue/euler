d964510d2a6576ae379be93b9af0476afe4bdfc3
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-07-13 15:32:14 +0200

    scala: 021

--- scala/src/euler/Prob014.scala
+++ scala/src/euler/Prob014.scala
@@ -4,7 +4,7 @@ import util.Collatz
 
 object Prob014 extends App {
   val N = 1000000
-  val res = ((1 to N) map (i => (i, Collatz.seqLen(i)))).maxBy(_._2)
+  val res = ((N to 1 by -1) map (i => (i, Collatz.seqLen(i)))).maxBy(_._2)
 
   println(res)
   // -> (837799,525)
